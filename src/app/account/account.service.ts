fbae35424b400c98c6ae1baad6aa98d9985a4e9a
Fazle Rabbi <fazlerabbi2008@yahoo.com>
2023-02-18 19:44:23 +0600

    add logout, view name after login

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs';
-import { IUser } from './IUser';
+import { BehaviorSubject, map } from 'rxjs';
+import { IUser } from '../models/IUser';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<IUser | any>(null);
+
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) {}
+
+  getCurrentUserValue() {
+    return this.currentUserSource.value;
+  }
 
   register(values: any) {
     return this.http.post(this.baseUrl + 'Auth/register', values).pipe(
@@ -27,11 +34,16 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'Auth/login', values).pipe(
       map((user: any): void => {
         if (user) {
-          console.log(user);
-
           localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);  
         }
       })
     );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
   }
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AccountService } from '../account/account.service';
+import { IUser } from '../models/IUser';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  currentUser$!: Observable<IUser>;
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
+  logout() {
+    this.accountService.logout();
+  }
 }
